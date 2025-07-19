a3d4704f6c87ac73e48111f00378f2dfafed5efe
puchigire <83187087+puchigire@users.noreply.github.com>
2025-07-19 14:01:07 -0700

    Update and rename test18.js to test19.js

--- test19.js
+++ test19.js
@@ -428,3 +428,9 @@ $("#messagebuffer a").parent().parent().each(function () {
         , "https://raw.githubusercontent.com/om3tcw/r/emotes/emotes/anyatoast2.png"
     ];
 
+    const drawRandomEmote = () => randomEmotePool[Math.floor(Math.random() * randomEmotePool.length)];
+
+    $("#emotelistbtn").click(function () {
+        $(this).css("background-image", "url(" + drawRandomEmote() + ")");
+    }).html("");
+})();
